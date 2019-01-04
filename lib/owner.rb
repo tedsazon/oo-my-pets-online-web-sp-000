36860e17a46f59cedd178f1d155227afca67df6e
Ted Sazon <tedsazon@gmail.com>
2019-01-04 20:16:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,18 +1,10 @@
 class Owner
-  attr_accessor :name
-  attr_reader :species
-  attr_writer
 
- OWNERS = []
 
-  def initialize(species)
-    @species = species
-    OWNERS << self
-  end
 
-  def say_species
-    "I am a #{species}"
-  end
+
+
+
 
 
 end
