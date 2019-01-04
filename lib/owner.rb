900802ea1626e2d6664b54d9c4423377e9e285ba
Ted Sazon <tedsazon@gmail.com>
2019-01-04 20:38:16 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -6,7 +6,7 @@ class Owner
 
   def initialize(species)
     @species = species
-    @@owners << self
+    OWNERS << self
     @pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
@@ -22,7 +22,7 @@ class Owner
     "I am a #{species}."
   end
 
-  
+
 
 
 
